2cb546f828b826220abecf477b1c2735700acc26
Ben Sharafian <benms64@gmail.com>
2016-08-25 17:08:39 -0700

    refactor: change name used by plugin-virtual debug

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd $(dirname $0)/../node_modules/five-bells-connector
-DEBUG=connection,connection:err,plugin,plugin:err npm start
+DEBUG="connection,connection:err,ilp-plugin-virtual,ilp-plugin-virtual:err" npm start
